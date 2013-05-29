50dd69e5034562e138096deb944df32434768f6f
Jannis Gundermann <jannis@gundermann.me>
2013-05-30 09:02:08 +1200

    Bump to add-on version number in accordance to issue #2.

--- hooks.pagereorder.php
+++ hooks.pagereorder.php
@@ -3,7 +3,7 @@ class Hooks_pagereorder extends Hooks {
 
   public $meta = array(
      'name'       => 'Page Reorder',
-     'version'    => '0.1.2',
+     'version'    => '0.1.3',
      'author'     => 'Jannis Gundermann',
      'author_url' => 'http://jannisgundermann.com'
    );
